a802f47717671c30adbbda1aa691ace6bd782062
haibang497 <haibang497@gmail.com>
2020-09-17 10:45:36 +0700

    Updates

---
+++ precache-manifest.b52b868d8d515db7ec0e43ccf66c4ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e232528a77c0a74eecc113cebe962d7a",
+    "url": "/ToDoList-ReactApp/index.html"
+  },
+  {
+    "revision": "cffd7ecc35500f479396",
+    "url": "/ToDoList-ReactApp/static/css/main.40e0842c.chunk.css"
+  },
+  {
+    "revision": "58a48bfd4fb8527d46a3",
+    "url": "/ToDoList-ReactApp/static/js/2.9b02a678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList-ReactApp/static/js/2.9b02a678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cffd7ecc35500f479396",
+    "url": "/ToDoList-ReactApp/static/js/main.bf145a25.chunk.js"
+  },
+  {
+    "revision": "005e556f1658f38cb67d",
+    "url": "/ToDoList-ReactApp/static/js/runtime-main.43ff4355.js"
+  }
+]);
